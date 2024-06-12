5f07e219dc656a9ad02d8cc292e74164413c05b2
Anna Sarabasheva <162803681+annasarabasheva@users.noreply.github.com>
2024-06-12 20:38:13 +0300

    Delete Build-In Functions Lab directory

--- Build-In Functions Lab/Format_Date_of_Birth.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-	last_name AS "Last Name",
-	TO_CHAR(born, 'DD (Dy) Mon YYYY') as "Date of Birth"
-FROM authors;
--- Build-In Functions Lab/Year_of_Birth.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-	first_name,
-	last_name,
-	extract('year' from born) AS "year"
-FROM authors
