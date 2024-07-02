62ffceee0598c9ada94e3c0ca066c347a3bd1c64
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-07-02 19:06:28 +0300

    Create Count_Employees_by_Town.sql

---
+++ Database Programmability Lab/Count_Employees_by_Town.sql
@@ -0,0 +1,5 @@
+CREATE FUNCTION fn_count_employees_by_town(town_name VARCHAR(20)) RETURNS INT AS $$ DECLARE e_count INT; BEGIN
+return type declare local variable
+SELECT COUNT(employee_id) INTO e_count FROM employees AS e JOIN addresses AS a ON a.address_id = e.address_id JOIN towns AS t ON t.town_id = a.town_id WHERE t.name = town_name; RETURN e_count;
+return value
+END; $$ LANGUAGE plpgsql;
