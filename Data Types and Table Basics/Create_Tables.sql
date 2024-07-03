c74ace6f04a35243c43f33d33ca95c7505c4d67d
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-07-03 18:24:35 +0300

    Rename Table

---
+++ Data Types and Table Basics/Create_Tables.sql
@@ -0,0 +1,29 @@
+CREATE TABLE employees(
+		id serial PRIMARY KEY NOT NULL,
+		first_name VARCHAR(30),
+		last_name VARCHAR(50),
+		hiring_date date DEFAULT '2023-01-01',
+		salary numeric(10,2),
+		devices_number integer
+
+
+);
+
+CREATE TABLE departments(
+		id serial PRIMARY KEY NOT NULL,
+		"name" VARCHAR(50),
+		code CHAR(3),
+		description text
+
+
+);
+
+
+CREATE TABLE issues(
+		id serial PRIMARY KEY UNIQUE,
+		description VARCHAR(150),
+		"date" date,
+		"start" TIMESTAMP
+
+
+);
